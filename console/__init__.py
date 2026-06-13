78ff4ab2341f839b25cefce2e002a92ba21644b5
Aviv <54484588+avivper@users.noreply.github.com>
2026-06-13 03:09:23 +0300

    Delete console directory

--- console/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Console package for managing the interactive REPL.
-Contains the main Session loop, shared AppState, and custom exceptions.
-"""
-
-from .session import Session
-from .app_state import AppState
